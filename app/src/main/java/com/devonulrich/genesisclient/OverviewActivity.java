a38a251978cf17f0d3983972cab208edb8005054
Devon Ulrich <dpulrich@gmail.com>
2015-09-20 13:50:26 -0400

    Removed unnecessary menu files

--- app/src/main/java/com/devonulrich/genesisclient/OverviewActivity.java
+++ app/src/main/java/com/devonulrich/genesisclient/OverviewActivity.java
@@ -5,11 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
-import com.devonulrich.genesisclient.login.LoginStore;
 import com.devonulrich.genesisclient.network.OverviewTask;
 
 import jp.wasabeef.recyclerview.animators.FadeInLeftAnimator;
@@ -52,31 +48,5 @@ public class OverviewActivity extends Activity {
         i.addCategory(Intent.CATEGORY_HOME);
         i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(i);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_overview, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        } else if (id == R.id.reset_all) {
-            LoginStore.reset(this);
-            Toast.makeText(this, "All Saved Settings Deleted", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
