6925ffc1b110fa8e433facc86774f4ebf564cc9d
Devon Ulrich <dpulrich@gmail.com>
2016-05-15 11:12:20 -0400

    Changed the notification file structure
    Instead of having lots of small files for each class, the app
    now just uses a single large file that saves the  previous
    amounts for every class.

--- app/src/main/java/com/devonulrich/genesisclient/data/files/NotificationStore.java
+++ app/src/main/java/com/devonulrich/genesisclient/data/files/NotificationStore.java
@@ -5,22 +5,20 @@ import android.content.SharedPreferences;
 
 public class NotificationStore {
 
-    private static final String NOTIFIC_PREFS_NAME = "com.devonulrich.genesisclient.notific-";
-
-    private static final String ASSIGNMENT_COUNT_KEY = "ass-count";
+    private static final String NOTIFIC_PREFS_NAME = "com.devonulrich.genesisclient.notification";
 
     public static int getNotificationInfo(Context c, String classID) {
         //note; the classID string must be with a colon instead of a slash
         SharedPreferences sp = c.getSharedPreferences(
-                NOTIFIC_PREFS_NAME + classID, Context.MODE_PRIVATE);
-        return sp.getInt(ASSIGNMENT_COUNT_KEY, 2147483647);
+                NOTIFIC_PREFS_NAME, Context.MODE_PRIVATE);
+        return sp.getInt(classID, 2147483647);
     }
 
     public static void setNotificationInfo(Context c, String classID, int assignmentCount) {
         SharedPreferences sp = c.getSharedPreferences(
-                NOTIFIC_PREFS_NAME + classID, Context.MODE_PRIVATE);
+                NOTIFIC_PREFS_NAME, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
-        editor.putInt(ASSIGNMENT_COUNT_KEY, assignmentCount);
+        editor.putInt(classID, assignmentCount);
         editor.apply();
     }
 }
